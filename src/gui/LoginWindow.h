184c3015e0761a67649767322866a3d5b5eca200
lamara <lamara_ihab_saeed@aucegypt.edu>
2026-04-18 18:30:00 +0200

    renaming windows +completing gui code

---
+++ src/gui/LoginWindow.h
@@ -0,0 +1,35 @@
+#ifndef LOGINWINDOW_H
+#define LOGINWINDOW_H
+
+#include <QWidget>
+
+namespace Ui {
+class LoginWindow;
+}
+
+class LoginWindow : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit LoginWindow(QWidget *parent = nullptr);
+    ~LoginWindow();
+
+signals:
+    void loginRequested(const QString &username, const QString &password);
+    void switchToRegister();
+
+public slots:
+    void showError(const QString &message);
+    void showLoading(bool loading);
+    void clearFields();
+
+private slots:
+    void onLoginButtonClicked();
+
+private:
+    Ui::LoginWindow *ui;
+};
+
+#endif
+
