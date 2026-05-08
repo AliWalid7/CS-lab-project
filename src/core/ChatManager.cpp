bd23b916f4327dc276754f521e30ce6389d8325f
lamara <lamara_ihab_saeed@aucegypt.edu>
2026-05-08 16:29:53 +0300

    ali and omar business logic

--- src/core/ChatManager.cpp
+++ src/core/ChatManager.cpp
@@ -1,36 +1,20 @@
 #include "ChatManager.h"
 
-#include <QJsonDocument>
-#include <QJsonObject>
+ChatManager::ChatManager(QObject *parent) : QObject(parent) {}
 
-ChatManager::ChatManager() {}
-
-QByteArray ChatManager::joinMessage(const QString &username)
+bool ChatManager::addMessage(const QString& username, const QString& text)
 {
-    QJsonObject obj;
-    obj["type"] = "join";
-    obj["username"] = username;
-    return QJsonDocument(obj).toJson();
-}
-
-QByteArray ChatManager::chatMessage(const QString &username, const QString &text)
-{
-    QJsonObject obj;
-    obj["type"] = "message";
-    obj["username"] = username;
-    obj["text"] = text;
-    return QJsonDocument(obj).toJson();
-}
-
-void ChatManager::addToHistory(const QString &username, const QString &text)
-{
+    if (username.isEmpty() || text.isEmpty())
+        return false;
     Message msg;
     msg.username = username;
     msg.text = text;
     m_history.append(msg);
+    historyChanged(m_history);
+    return true;
 }
 
-QList<Message> ChatManager::getHistory()
+QList<Message> ChatManager::getHistory() const
 {
     return m_history;
 }
@@ -38,4 +22,42 @@ QList<Message> ChatManager::getHistory()
 void ChatManager::clearHistory()
 {
     m_history.clear();
+    historyChanged(m_history);
+}
+
+bool ChatManager::addUser(const QString& username)
+{
+    if (username.isEmpty())
+        return false;
+    QString lower = username.toLower();
+    for (const QString& u : m_users)
+        if (u.toLower() == lower)
+            return false;
+    m_users.append(username);
+    usersChanged(m_users);
+    return true;
+}
+
+bool ChatManager::removeUser(const QString& username)
+{
+    QString lower = username.toLower();
+    for (int i = 0; i < m_users.size(); i++) {
+        if (m_users[i].toLower() == lower) {
+            m_users.removeAt(i);
+            usersChanged(m_users);
+            return true;
+        }
+    }
+    return false;
+}
+
+QStringList ChatManager::getUsers() const
+{
+    return m_users;
+}
+
+void ChatManager::clearUsers()
+{
+    m_users.clear();
+    usersChanged(m_users);
 }
